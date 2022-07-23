8e5e4fd8d8bb824b160e75b9db02c6ce9ee52a03
Caro Alonso <103946904+Caarito@users.noreply.github.com>
2022-07-23 01:13:23 -0300

    Update p1.p1.pas

--- Practicas/Practica 1/Resolución Practica/p1.p1.pas
+++ Practicas/Practica 1/Resolución Practica/p1.p1.pas
@@ -1,10 +1,15 @@
+{1. Realice un programa que lea 2 números enteros desde teclcado e informe en pantalla cuál
+de los dos números es el mayor. Si son iguales debe informar en pantalla lo siguiente: “Los
+números leídos son iguales”}
 
 program p1.p1;
 var
 num1:integer;
 num2:integer;
 begin 
+writeln('Ingrese numero 1 :');
 readln(num1);
+writeln('Ingrese numero 2 :')
 readln(num2);
 if (num1 = num2) then 
   writeln('Los numeros leidos son iguales')

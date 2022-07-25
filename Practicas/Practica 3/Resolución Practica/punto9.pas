a19255597e75fc835b2f05bd9b6bb605466413d5
Caro Alonso <103946904+Caarito@users.noreply.github.com>
2022-07-25 18:52:45 -0300

    Update punto9.pas

--- Practicas/Practica 3/Resolución Practica/punto9.pas
+++ Practicas/Practica 3/Resolución Practica/punto9.pas
@@ -1,7 +1,7 @@
 { 9.Realizar un programa que lea una secuencia de caracteres y verifique si cumple con el patrón A$B#  donde:
--	A es una secuencia de sólo letras vocales.
--	B es una secuencia de sólo caracteres alfabéticos sin letras vocales
--	los caracteres $ y # seguro existen
+-A es una secuencia de sólo letras vocales.
+-B es una secuencia de sólo caracteres alfabéticos sin letras vocales
+-los caracteres $ y # seguro existen
 
 Nota: en caso de no cumplir, informar que parte del patrón no se cumplió.
 
